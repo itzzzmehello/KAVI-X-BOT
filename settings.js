ed553999687e89f2d62cd28fcfec024052fdc6cd
James <adithyadimuth574@gmail.com>
2025-07-05 12:13:27 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -13,7 +13,7 @@ global.author = process.env.AUTHOR || "Cyber Kavi"
 global.creator = process.env.CREATOR || "94702128378@s.whatsapp.net"
 global.botprefix = process.env.BOTPREFIX || "."
 global.restart = process.env.RESTART || true
-global.mongoDB = process.env.MONGODB_URI || "mongodb+srv://adithyadimuth574:VEEtEzkV5XX0rYNO@cluster0.ukzryuj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+global.mongoDB = process.env.MONGODB_URI || "mongodb+srv://adithyadimuth574:adithyadimuth574@cluster0.ukzryuj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
